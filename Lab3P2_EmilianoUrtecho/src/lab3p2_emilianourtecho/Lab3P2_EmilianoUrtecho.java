9eaff9db22872fa29320414304b9a8f397bbb93d
Lab1P1_EmilianoUrtecho <emilio.urtechon@gmail.com>
2023-10-27 13:38:54 -0600

    1 ss

--- Lab3P2_EmilianoUrtecho/src/lab3p2_emilianourtecho/Lab3P2_EmilianoUrtecho.java
+++ Lab3P2_EmilianoUrtecho/src/lab3p2_emilianourtecho/Lab3P2_EmilianoUrtecho.java
@@ -1,20 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package lab3p2_emilianourtecho;
 
-/**
- *
- * @author emili
- */
-public class Lab3P2_EmilianoUrtecho {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Lab3P2_EmilianoUrtecho {
+    
+    static Scanner escan = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        int eleccion;
+        System.out.println("        ~MENU DE PRUEBA~");
+        System.out.println("1. Para el Administrador");
+        System.out.println("2. Para el Usuario");
+        System.out.println("3. SALIR");
+        System.out.print("Elija una opcion: ");
+        eleccion= escan.nextInt();
+        
+        do {
+            switch(eleccion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3: // 
+                    break;
+                default: // NO SE
+                    break;
+            }
+        } while (eleccion != 3);
     }
     
 }
